9f6ae94cbf0bd6f3da9b6e09a83d915066d906f5
christian@baltzer.de <christian@baltzer.de>
2019-10-10 13:49:37 +0200

    init request

---
+++ src/js/Extra/request.js
@@ -0,0 +1,44 @@
+/**
+ * Pull-request to the given Link
+ * @param {String} Link
+ * @param {String} Message
+ */
+export function Pull (Link, Message) {
+// TODO: Pull-function
+}
+
+/**
+ * Push-request to the given Link
+ * @param {String} Link
+ * @param {String} Message
+ */
+export function Push (Link, Message) {
+// TODO: Push-function
+}
+
+/**
+ * Put-request to the given Link
+ * @param {String} Link
+ * @param {String} Message
+ */
+export function Put (Link, Message) {
+// TODO: Put-function
+}
+
+/**
+ * Get-request to the given Link
+ * @param {String} Link
+ * @param {String} Message
+ */
+export function Get (Link, Message) {
+// TODO: Get-function
+}
+
+/**
+ * Delete-request to the given Link
+ * @param {String} Link
+ * @param {String} Message
+ */
+export function Delete (Link, Message) {
+// TODO: Delete-function
+}
