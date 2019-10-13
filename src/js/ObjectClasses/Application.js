1634c7b1c22749768839000f94ee218a4f44f21a
christian@baltzer.de <christian@baltzer.de>
2019-10-13 12:40:39 +0200

    Application Object added

---
+++ src/js/ObjectClasses/Application.js
@@ -0,0 +1,12 @@
+class Application {
+  constructor (name, ID, PID, Info, ICON, PositionX, PositionY, loopFunction) {
+    this.name = name
+    this.ID = ID
+    this.PID = PID
+    this.Info = Info
+    this.Icon = ICON
+    this.PositionX = PositionX
+    this.PositionY = PositionY
+    this.function = loopFunction
+  }
+}
