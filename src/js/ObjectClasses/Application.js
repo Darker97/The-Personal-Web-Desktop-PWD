8e501906b6af91eec06848a015680bf3fe9bf457
christian@baltzer.de <christian@baltzer.de>
2019-10-13 12:43:04 +0200

    added Doku

--- src/js/ObjectClasses/Application.js
+++ src/js/ObjectClasses/Application.js
@@ -1,4 +1,15 @@
 class Application {
+  /**
+   * Constructor for the Application Object
+   * @param {String} name 
+   * @param {int} ID 
+   * @param {int} PID 
+   * @param {String} Info 
+   * @param {Url} ICON 
+   * @param {int} PositionX 
+   * @param {int} PositionY 
+   * @param {function} loopFunction 
+   */
   constructor (name, ID, PID, Info, ICON, PositionX, PositionY, loopFunction) {
     this.name = name
     this.ID = ID
--- src/js/ObjectClasses/User.js
+++ src/js/ObjectClasses/User.js
@@ -1,4 +1,11 @@
 class User {
+/**
+ * Constructor for the User Objects
+ * @param {String} name
+ * @param {String} Password
+ * @param {URL} Picture
+ * @param {String} UserName
+ */
   constructor (name, Password, Picture, UserName) {
     this.name = name
     this.Password = Password
