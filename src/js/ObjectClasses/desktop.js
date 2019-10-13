ee57dc5dad19d655e70f0ec5fef4fc9ab8d7151c
christian@baltzer.de <christian@baltzer.de>
2019-10-13 16:13:22 +0200

    added a desktop Class

---
+++ src/js/ObjectClasses/desktop.js
@@ -0,0 +1,13 @@
+class desktop {
+//**
+    * Builds a constructor for the desktop Variables
+    * @param {int} sizeX 
+    * @param {int} sizeY 
+    * @param {String} Backround 
+    * /
+  constructor (sizeX, sizeY, Backround) {
+    this.sizeX = sizeX
+    this.sizeY = sizeY
+    Backround = this.Backround
+  }
+}
