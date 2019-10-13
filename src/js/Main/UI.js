2dc95cd1c21bcdd9c585b6398af73466b986667b
christian@baltzer.de <christian@baltzer.de>
2019-10-13 12:43:55 +0200

    added doku and a new function

--- src/js/Main/UI.js
+++ src/js/Main/UI.js
@@ -1,3 +1,10 @@
 export function MenüLeiste () {
 // Todo: Menüleiste init
 }
+/**
+ * Builds a standart Object for the App Window
+ * @param {String} Name
+ */
+export function AppStandart (Name, coordinateX, coordinateY) {
+
+}
