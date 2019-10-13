26960ebab3490f4c1716ee93a5487d6cbba68793
christian@baltzer.de <christian@baltzer.de>
2019-10-13 12:40:30 +0200

    User Object added

---
+++ src/js/ObjectClasses/User.js
@@ -0,0 +1,8 @@
+class User {
+  constructor (name, Password, Picture, UserName) {
+    this.name = name
+    this.Password = Password
+    this.Picture = Picture
+    this.UserName = UserName
+  }
+}
