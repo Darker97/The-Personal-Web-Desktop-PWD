569afcb8e2757ef5200a500f4345d6f0b43bbf42
christian@baltzer.de <christian@baltzer.de>
2019-10-16 14:23:57 +0200

    linter fix

--- src/js/ObjectClasses/Application.js
+++ src/js/ObjectClasses/Application.js
@@ -1,14 +1,14 @@
 export class Application {
   /**
    * Constructor for the Application Object
-   * @param {String} name 
-   * @param {int} ID 
-   * @param {int} PID 
-   * @param {String} Info 
-   * @param {Url} ICON 
-   * @param {int} PositionX 
-   * @param {int} PositionY 
-   * @param {function} loopFunction 
+   * @param {String} name
+   * @param {int} ID
+   * @param {int} PID
+   * @param {String} Info
+   * @param {Url} ICON
+   * @param {int} PositionX
+   * @param {int} PositionY
+   * @param {function} loopFunction
    */
   constructor (name, ID, PID, Info, ICON, PositionX, PositionY, loopFunction) {
     this.name = name
