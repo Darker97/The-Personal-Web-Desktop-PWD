28705798c26883e50d5d781b73a51a956090a5c2
christian@baltzer.de <christian@baltzer.de>
2019-10-17 16:17:55 +0200

    small fixes

---
+++ src/js/App/test.js
@@ -0,0 +1,19 @@
+export function setup () {
+  return UI()
+}
+
+export function loop () {
+
+}
+
+export function kill () {
+
+}
+
+function UI () {
+  return document.createElement('p')
+}
+
+export function minimising () {
+
+}
