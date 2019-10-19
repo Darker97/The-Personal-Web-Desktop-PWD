adf589a296d1b90895dd8498df45cdfdb354963a
christian@baltzer.de <christian@baltzer.de>
2019-10-19 22:46:48 +0200

    updated the Application template

--- src/Template/App.js
+++ src/Template/App.js
@@ -1,20 +1,40 @@
+/**
+ * Everything that your App needs to do first
+ * Load save states, create the UI etc.
+ */
 export function setup () {
-  // returns the UI Object.
-  return null
+  return UI()
 }
 
-export function loop (app) {
-
+/**
+ * used for all functions that have to be checked regulary
+ * @param {Application} Application
+ */
+export function loop (Application) {
 }
 
-export function kill () {
-
+/**
+ * deletes the element of the App
+ * @param {Application} app
+ */
+export function kill (app) {
+  console.log(app.name + ' kill')
+  const temp = app.workinkObject.parentElement
+  temp.parentElement.removeChild(temp)
 }
 
-export function UI () {
-
+/**
+ * Creates the UI of the APP
+ */
+function UI () {
+  const temp = document.createElement('div')
+  // YOUR CODE HERE
+  return temp
 }
 
+/**
+ * not yet in use
+ */
 export function minimising () {
 
 }
