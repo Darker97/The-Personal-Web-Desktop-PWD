cc40e3af570481316b897f1283ac44c5070c3cfa
christian@baltzer.de <christian@baltzer.de>
2019-10-24 15:48:06 +0200

    added the app to the system

--- src/js/Main/Infobuild.js
+++ src/js/Main/Infobuild.js
@@ -3,6 +3,7 @@ import * as test from '../App/test.js'
 import * as Memmory from '../App/Memmory.js'
 import * as Chat from '../App/Chat.js'
 import * as never from '../App/never.js'
+import * as Bird from '../App/FlappyOrigami.js'
 
 export function appInit () {
   const application = []
@@ -10,5 +11,6 @@ export function appInit () {
   application.push(new Application('Memmory', 2, 0, 'memmory Game', 'Memmory.png', 0, 0, function (app) { Memmory.loop() }, function (app) { return Memmory.setup(app) }, function (app) { Memmory.kill(app) }, function (element, app) { Memmory.focus(element, app) }, function (app) { Memmory.focus(app) }))
   application.push(new Application('Chat', 3, 0, 'ChatApp', 'chat.png', 0, 0, function (app) { Chat.loop() }, function (app) { return Chat.setup(app) }, function (app) { Chat.kill(app) }, function (element, app) { Chat.focus(element, app) }, function (app) { Chat.focus(app) }))
   application.push(new Application('Never', 4, 0, 'He will never...', 'never.png', 0, 0, function (app) { never.loop(app) }, function (app) { return never.setup(app) }, function (app) { never.kill(app) }, function (element, app) { never.focus(element, app) }, function (app) { never.focus(app) }))
+  application.push(new Application('Bird', 4, 0, 'Everybody jump', 'Bird.png', 0, 0, function (app) { Bird.loop(app) }, function (app) { return Bird.setup(app) }, function (app) { Bird.kill(app) }, function (element, app) { Bird.focus(element, app) }, function (app) { Bird.focus(app) }))
   return application
 }
