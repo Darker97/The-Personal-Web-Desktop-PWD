db887ea2c9d1301a11193d875bfc3c6dbab711f7
christian@baltzer.de <christian@baltzer.de>
2019-10-24 14:44:13 +0200

    init

---
+++ src/js/App/FlappyOrigami.js
@@ -0,0 +1,66 @@
+import * as game from "./FlappyOrigami src/Game.js";
+
+/**
+ * Everything that your App needs to do first
+ * Load save states, create the UI etc.
+ */
+export function setup (app) {
+  // YOUR CODE HERE
+  return UI()
+}
+
+/**
+     * used for all functions that have to be checked regulary
+     * @param {Application} Application
+     */
+export function loop (Application) {
+  // YOUR CODE HERE
+}
+
+/**
+     * deletes the element of the App
+     * @param {Application} app
+     */
+export function kill (app) {
+  console.log(app.name + ' kill')
+  app.KeyCheck = false
+  const temp = app.workinkObject.parentElement
+  temp.parentElement.removeChild(temp)
+}
+
+/**
+     * Creates the UI of the APP
+     */
+function UI (app) {
+  const temp = document.createElement('div')
+  temp.id = 'FlappyOrigami'
+  game.UI(temp)
+  return temp
+}
+
+/**
+     * not yet in use
+     */
+export function minimising () {
+
+}
+
+/**
+     * If the App gets in Focus, this function should focus the right things
+     *  */
+export function focus (element, app) {
+  app.KeyCheck = true
+}
+
+function addKeyboard (app, temp) {
+  window.addEventListener('keydown', function (event) {
+    if (app.KeyCheck === true) {
+      app.CardArray.forEach(element => {
+        if (element.key === event.key) {
+          element.TurnFunction(element, temp, app)
+        }
+      })
+    }
+  })
+}
+
